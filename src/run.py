c9ec94a7f91c1b19168fadd26832567f187847b0
nzagdansk <nzagdansk@univ.haifa.ac.il>
2021-10-05 17:07:11 +0300

    gen scene from py2

--- src/run.py
+++ src/run.py
@@ -1,11 +1,20 @@
 from simulation_controller import SimulationController
-from scene_generator import SceneGenerator
+from scene_generator import SceneGenerator, ColorMap
 
 
 
 
 def main():
     print('hello')
+
+
+    seabed_depth = 15
+    gen = SceneGenerator(seabed_depth)
+    gen.add_empty_frame(ColorMap.jet)
+    gen.add_reefs()
+    scene = gen.generate()
+    print(scene)
+
 
 if __name__=='__main__':
     main()
