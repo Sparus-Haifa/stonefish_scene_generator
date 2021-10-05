12b9e17b2d2c34b8742a111f8213574ba2743b0a
nzagdansk <nzagdansk@univ.haifa.ac.il>
2021-10-05 14:16:49 +0300

    stonefish timout
    starting timer when reciving a topic (ROS and sim are up)

--- src/run.py
+++ src/run.py
@@ -1,25 +1,93 @@
 #!/usr/bin/env python
 
+from sys import stdin, version
+import os
+
+if version.startswith('3'):
+    print('python 3 not supported')
+    exit(1)
+
 import rospy
-import subprocess
-import signal
+import rospkg
+import roslaunch
+# import time
 
-  
-child = subprocess.Popen(["roslaunch","cola2_stonefish","sparus2_haifa_deepersense_simulation.launch"], stdout=subprocess.PIPE)
-for c in iter(lambda: child.stdout.readline(), b''): 
-    print('line', c)
-    if c.startswith('[INFO]'):
-        break
-# child.wait() #You can use this line to block the parent process untill the child process finished.
-print("parent process")
-print(child.poll())
+from sensor_msgs.msg import CameraInfo
 
-rospy.loginfo('The PID of child: %d', child.pid)
-print ("The PID of child:", child.pid)
 
-rospy.sleep(15)
+class SimulationController:
+    def __init__(self):
+        self.ros_is_running = False
+        self.timer = None
+        
+        uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
+        roslaunch.configure_logging(uuid)
 
-child.send_signal(signal.SIGINT) #You may also use .terminate() method
-#child.terminate()
 
-#for more: https://docs.python.org/2/library/subprocess.html
+        def getPath():
+            # get path
+            # get an instance of RosPack with the default search paths
+            rospack = rospkg.RosPack()
+
+            # list all packages, equivalent to rospack list
+            # rospack.list() 
+
+            # get the file path for rospy_tutorials
+            package_path = rospack.get_path('stonefish_scene_generator')
+
+
+            full_path = os.path.join(package_path, 'launch','sparus2_haifa_deepersense_simulation.launch')
+            return full_path
+
+        full_path = getPath()
+        self.launch = roslaunch.parent.ROSLaunchParent(uuid, [full_path])
+
+    def run(self):
+        self.launch.start()
+        rospy.loginfo("started")
+
+    
+    def wait_for_ros(self):
+        rospy.init_node('deepersense', anonymous=True)
+        
+
+        def callback(data):
+            if self.timer is None:
+                rospy.loginfo('got ROS topic (camera info). starting timer')
+                self.timer = rospy.get_time()
+                return
+
+            
+            
+        
+        rospy.Subscriber("/camera2/image_raw/camera_info", CameraInfo, callback, queue_size=1)
+
+
+
+        r = rospy.Rate(10) # 10hz
+        
+        while not rospy.is_shutdown():
+            if not self.timer is None:
+                if rospy.get_time() - self.timer > 15:
+                    break
+            r.sleep()
+
+
+
+        rospy.signal_shutdown('timeout')
+        self.launch.shutdown()
+
+
+
+
+def main():
+
+
+
+    controller = SimulationController()
+    controller.run()
+    controller.wait_for_ros()
+
+
+if __name__=='__main__':
+    main()
